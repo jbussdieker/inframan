455a409c767a9686c2ae908704a0dee1dcbc7b1d
Joshua Bussdieker <jbussdieker@gmail.com>
2013-01-22 00:52:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inframan::Application.config.secret_token = 'ed8874e63d34fec649dd719a1d3963803338c5ddba375e0f00e8e27ea8ca8dc791fa91f2a5a6ed089ce1a84a23e3edf3e9dc61aa80f3139f1bdc64cfb66decbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Inframan::Application.config.session_store :cookie_store, key: '_inframan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Inframan::Application.config.session_store :active_record_store
