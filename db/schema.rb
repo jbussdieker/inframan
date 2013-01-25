a5737d96947b4a0738e660084abfe589bcc6aa20
Joshua Bussdieker <jbussdieker@gmail.com>
2013-01-24 23:38:49 -0800

    New image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124070135) do
+ActiveRecord::Schema.define(:version => 20130124062037) do
 
   create_table "providers", :force => true do |t|
     t.string   "name"
@@ -28,12 +28,6 @@ ActiveRecord::Schema.define(:version => 20130124070135) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "roles", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "servers", :force => true do |t|
     t.string   "name"
     t.string   "public_ip"
@@ -43,12 +37,6 @@ ActiveRecord::Schema.define(:version => 20130124070135) do
     t.string   "image_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "types", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
